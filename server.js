768a3fb0dd5905f87b1a07986e36db29a6505284
unknown <digitaltechpagamentos@gmail.com>
2026-03-27 15:19:12 -0300

    add excluir cliente

--- server.js
+++ server.js
@@ -150,6 +150,30 @@ return res.status(500).json({ erro: error.message });
 res.json(data);
 });
 
+/* ================= EXCLUIR CLIENTE ================= */
+
+app.post("/excluir-cliente", async (req, res) => {
+
+const { id } = req.body;
+
+if (!id) {
+return res.status(400).json({ erro: "ID não informado" });
+}
+
+const { error } = await supabase
+.from("clientes")
+.delete()
+.eq("id", id);
+
+if (error) {
+console.log("ERRO AO EXCLUIR CLIENTE:", error);
+return res.status(500).json({ erro: error.message });
+}
+
+res.json({ mensagem: "Cliente excluído com sucesso" });
+
+});
+
 /* ================= REVENDEDORES ================= */
 
 // 🔥 CRIAR REVENDEDOR
